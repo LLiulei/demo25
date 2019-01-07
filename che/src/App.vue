7d9d75ad98fb2f1cd289181f2b6a073ff5380e29
LLiulei <Liujingting329@163.com>
2019-01-07 12:03:52 +0800

    响应式布局里面吸顶效果

--- che/src/App.vue
+++ che/src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="app">
-    <div id="header" ref="header">
+    <div id="header" ref="header" :class="state ? 'actH' : ''">
       <div id="tab">
         <h5>车轮</h5>
         <ul>
@@ -20,7 +20,7 @@
     </div>
     <div id="content">
         <ul class="tablist">
-          <li v-for="(item,index) in list" :key="index" @click="getTab(item.id)">{{item.name}}</li>
+          <li v-for="(item,index) in list" :key="index" @click="getTab(item.id)" :class="ind === item.id ? 'active' : ''">{{item.name}}</li>
         </ul>
         <Comment :class="ind === 1 ? 'show' : 'hide'"/>
         <Trend  :class="ind === 2 ? 'show' : 'hide'"/>
@@ -30,6 +30,8 @@
 </template>
 
 <script>
+  import BScroll from "better-scroll";
+  import Vue from "vue";
   import Comment from "./components/comment";
   import Trend from "./components/trend";
   import Report from "./components/report";
@@ -37,8 +39,8 @@
     name: 'app',
     data () {
       return {
-        curHeight: 0,
         ind: 1,
+        state: false,
         list: [{
           id: 1,
           name: "分析评论"
@@ -51,20 +53,15 @@
         }]
       }
     },
-    beforeMount(height) {
-        // let height = document.getElementById("header");
-        //let h = document.documentElement.clientHeight || document.body.clientHeight;
-        // this.curHeight = h - height; //减去页面上固定高度height
-        // let height= this.$refs.header;  //100
-        let header = document.getElementById("header");
-        console.log(this)
-        console.log(document.getElementById("header"))
+    beforeMount() {
+      
     },
     created() {
       this.getTab
+      this.scroll()
     },
     mounted() {
-
+      
     },
     components: {
       Comment,
@@ -74,156 +71,30 @@
     methods: {
       getTab(i) {
         this.ind = i;
+      },
+      scroll() {
+        // 获取到节点
+        // let header = document.getElementById("header");
+        // 滚动的距离
+        window.onscroll = (e) => {
+          let scrollTop = document.body.scrollTop || document.documentElement.scrollTop;
+          // 判断滚动的距离 改变状态
+          if(scrollTop >= 200) {
+            this.state = true;
+          }else{
+            this.state = false;
+          }
+        }
       }
     }
   }
 </script>
 
 <style lang="scss">
+  // 全局样式
   @import "./common/style.scss";
-  // 手机版
-  @media screen and (max-width: 1000px) {
-    #header {
-      width: 100%;
-      height: 0.9rem;
-      #tab {
-        width: 100%;
-        height: 0.9rem;
-        line-height: 0.9rem;
-        border-bottom: solid 1px rgba(50,50,50,.2);
-        display: flex;
-        h5 {
-          flex: 1;
-          font-size: 0.4rem;
-          padding-left: 0.5rem;
-        }
-        ul {
-          display: none;
-        }
-        #login {
-          display: none;
-        }
-        #more {
-          width: 10%;
-          text-align: center;
-          margin-right: 0.5rem;
-          color: blue;
-          font-size: 0.8rem;
-        }
-      }
-    }
-    #head { 
-      width: 100%;
-      height: 900px;
-      #tabImg {
-        width: 100%;
-        height: 100%;
-      }
-    }
-    #content {  
-      width: 100%;
-      height: 100%;
-    }
-    #footer {  }
-  }
-  // 电脑版
-  @media screen and (min-width: 1000px) {
-    #header {
-      position: fixed;
-      left: 0;
-      top: 0;
-      right: 0;
-      z-index: 100;
-      height: 1rem;
-      line-height: 1rem;
-      border-bottom: solid 1px #d6d6d6;
-      -webkit-transition: padding .3s,background-color .3s,box-shadow .3s,border-bottom .3s;
-      -moz-transition: padding .3s,background-color .3s,box-shadow .3s,border-bottom .3s;
-      transition: padding .3s,background-color .3s,box-shadow .3s,border-bottom .3s;
-      #tab {
-        width: 100%;
-        border-bottom: solid 1px rgba(50,50,50,.2);
-        display: flex;
-        h5{
-          flex: 1;
-          font-size: 0.5rem;
-          text-align: center;
-        }
-        ul{
-          flex: 4;
-          font-size: 0.2rem;
-          padding-left: 1rem;
-          box-sizing: border-box;
-          li{
-            padding: 0 0.08rem;
-            float: left;
-          }
-        }
-        #login{
-          flex: 1;
-          font-size: 0.1rem;
-          text-align: center;
-        }
-        #more{
-          display: none;
-        }
-      }
-    }
-    #head { 
-      position: relative;
-      margin: 0 auto;
-      height: 700px;
-      width: 100%;
-      max-width: 1920px;
-      #tabImg {
-        position: absolute;
-        left: 50%;
-        top: 0;
-        z-index: 10;
-        margin-left: -960px;
-        width: 1920px;
-        height: 100%;
-      }
-    }
-    #content {  
-      width: 100%;
-      height: 100%;
-      .tablist {
-        width: 100%;
-        height: 1rem;
-        line-height: 1rem;
-        font-size: 0.2rem;
-        border-bottom: 0.01rem solid #ccc;
-        color: #ccc;
-        display: flex;
-        li{
-          width: 20%;
-          text-align: center;
-          .active{
-            color: #000;
-            border-bottom: 0.03rem solid blue;
-          }
-        }
-      }
-      .hide {
-        display: none;
-      }
-      .show {
-        display: block;
-      }
-    }
-    #footer {  }
-  }
-  @media screen and (min-height: 731px) and (max-height: 800px){
-    #header {
-      height: 0.8rem;
-      line-height: 0.8rem;
-      z-index: 122;
-      background-color: #fff;
-      border-bottom: solid 1px #d6d6d6;
-      box-shadow: 0 2px 6px rgba(0,0,0,.1);
-      -webkit-backdrop-filter: blur(10px);
-      backdrop-filter: blur(10px);
-    }
-  }
+  // 手机版样式
+  @import "./common/phone.scss";
+  // 电脑版样式
+  @import "./common/computer.scss";
 </style>
--- che/src/components/trend.vue
+++ che/src/components/trend.vue
@@ -1,6 +1,21 @@
 <template>
     <div> 
-        趋势
+        <div class="trendLeft"></div>
+        <div class="trendRight">
+            <h6>关注指数排行榜</h6>
+            <ul>
+                <li>哈弗H6</li>
+                <li>本田CR-V</li>
+                <li>汉兰达</li>
+                <li>途观</li>
+                <li>缤智</li>
+                <li>本田XR-V</li>
+                <li>冠道</li>
+                <li>吉利博越</li>
+                <li>奔驰GLA级</li>
+                <li>全新途胜</li>
+            </ul>
+        </div>
     </div>
 </template>
 <script>
